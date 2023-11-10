250a85d05d35f08d174de64f5b5b37d791596aa7
Kulveer Singh <kulveer.coder@gmail.com>
2023-11-10 22:47:27 +0530

    testimonial and footer

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Layout from '../../components/layout/Layout';
 import myContext from '../../context/data/MyContext';
 import HeroSection from '../../components/heroSection/HeroSection';
 import Filter from '../../components/filter/Filter';
+import ProductCard from '../../components/productCard/ProductCard';
+import Testimonial from '../../components/testimonial/Testimonial';
 
 
 function Home() {
@@ -11,6 +13,8 @@ function Home() {
     <Layout>
      <HeroSection />
      <Filter />
+     <ProductCard />
+     <Testimonial />
     </Layout>
   )
 }
